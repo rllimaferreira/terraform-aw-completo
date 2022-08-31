f293e93ddec7bcff62c9d1ac9bee0e7374b30402
Nídio Dolfini <nidiosdolfini@gmail.com>
2022-08-31 18:38:12 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,4 @@
-output "instance_id" {
-    description = "ID das instancias"
-    value = aws_instance.front_nidio-ec2.*.id
-}
-
-output "instance_public_ip" {
+output "instance_front_public_ip" {
     description = "Ip publico"
     value = aws_instance.front_nidio-ec2.*.public_ip
 }
@@ -12,18 +7,24 @@ output "instance_front_private_ip" {
     value = aws_instance.front_nidio-ec2.*.private_ip
 }
 
-output "instance_public_dns" {
-    description = "DNS publico"
-    value = aws_instance.front_nidio-ec2.*.public_dns
-        
-}
-
-output "instance_private_ip" {
+output "instance_back_private_ip" {
     description = "Ip privado"
     value = aws_instance.back_nidio-ec2.*.private_ip
 }
 
-output "instance_public_ip_mngt" {
+output "instance_mngt_public_ip" {
     description = "Ip publico"
     value = aws_instance.mngt_nidio-ec2.*.public_ip
 }
+
+# output "instance_id" {
+#     description = "ID das instancias"
+#     value = aws_instance.front_nidio-ec2.*.id
+# }
+
+
+# output "instance_public_dns" {
+#     description = "DNS publico"
+#     value = aws_instance.front_nidio-ec2.*.public_dns
+        
+# }
